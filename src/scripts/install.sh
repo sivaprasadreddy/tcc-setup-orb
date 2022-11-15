cccad3e8653d6d02aa43e05de200a113a4717bb7
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-11-15 09:39:50 +0530

    Update to install TCC agent

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,7 +4,7 @@
 suffix=$([[ $(uname -m) == "aarch64" || $(uname -m) == "arm64" ]] && echo "arm64" || echo "x86-64")
 
 # fetch the latest TCC agent version and make it executable
-curl -L -o /tmp/agent https://app.testcontainers.cloud/download/testcontainers-cloud-agent_linux_$suffix
+curl -L -o /tmp/agent https://app.testcontainers.cloud/download/testcontainers-cloud-agent_linux_"$suffix"
 chmod +x /tmp/agent
 
 # start agent as background process
